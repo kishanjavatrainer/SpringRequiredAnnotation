1121af3c2d815a0420191e864fcbdc88e8c50ad0
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-03-07 20:49:27 +0530

    SpringRequiredAnnotation source code

---
+++ SpringRequiredAnnotation/src/com/infotech/model/Message.java
@@ -0,0 +1,25 @@
+package com.infotech.model;
+
+import org.springframework.beans.factory.annotation.Required;
+
+public class Message {
+
+	private int messageId;
+	private String message;
+	
+	public int getMessageId() {
+		return messageId;
+	}
+	public void setMessageId(int messageId) {
+		this.messageId = messageId;
+	}
+	public String getMessage() {
+		return message;
+	}
+	
+	@Required
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
